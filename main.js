eef7116e06f03ac1dae3f655735c063737e6fb25
taiyo8911 <68884687+taiyo8911@users.noreply.github.com>
2023-03-29 20:27:02 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// 地図を作成
+// 地図を表示する
 const map = L.map('map', {
     center: [35.4122, 139.4130], // 中心座標の指定
     zoom: 2, // ズームレベルの指定
@@ -16,7 +16,10 @@ const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.pn
     maxZoom: 18,
 }).addTo(map);
 
-// 座標(緯度、経度)の配列を用意
+
+
+// プロットを表示する
+// プロットする座標(緯度、経度)の配列を用意
 const markers = [
     { coordinates: [35.4122, 139.4130], country: 'Tokyo' },
     { coordinates: [28.3650, 77.1230], country: 'New Delhi' },
@@ -24,7 +27,46 @@ const markers = [
     { coordinates: [40.6643, 286.0615], country: 'New York' },
 ];
 
-// 国名からタイムゾーンを取得する
+
+// 各地点にプロットを追加
+markers.forEach(({ coordinates, country }, index) => {
+    // マーカーを作成
+    const marker = L.marker(coordinates).addTo(map);
+
+    // 時刻の表示
+    const timeElement = document.createElement('div');
+    const timeContainer = document.createElement('div');
+    timeContainer.appendChild(document.createTextNode(country.toUpperCase() + ' '));
+    timeContainer.appendChild(timeElement);
+    marker.bindTooltip(timeContainer, {
+        permanent: true, // ラベルを常時表示
+        direction: index === 3 ? 'top' : 'right', // ラベルの表示位置を設定
+        offset: [0, index === 3 ? -10 : 10], // ラベルの位置を微調整
+        className: 'custom-marker' // ラベルのスタイルを設定するCSSクラス名
+    });
+
+    // 時刻を更新する関数
+    const updateTime = () => {
+        const time = new Date().toLocaleTimeString('ja-JP', {
+            timeZone: getTimeZone(country),
+            hour12: false,
+            hour: '2-digit',
+            minute: '2-digit',
+            seconds: false,
+        });
+        timeElement.textContent = time;
+    };
+
+    // 初回表示
+    updateTime();
+
+    // 1秒ごとに時刻を更新する
+    setInterval(updateTime, 1000);
+});
+
+
+
+// 国名からタイムゾーンを取得する関数
 function getTimeZone(country) {
     switch (country.toLowerCase()) {
         case 'tokyo':
@@ -38,42 +80,4 @@ function getTimeZone(country) {
         default:
             return 'UTC';
     }
-}
-
-// 各地点にプロットを追加
-markers.forEach(({ coordinates, country }, index) => {
-    // マーカーを作成
-    const marker = L.marker(coordinates).addTo(map);
-
-    // 時刻の表示
-    const time = new Date().toLocaleTimeString('ja-JP', {
-        timeZone: getTimeZone(country),
-        hour12: false,
-        hour: '2-digit',
-        minute: '2-digit',
-        seconds: false,
-    });
-
-    // ラベルを作成
-    marker.bindTooltip(`<div>${country.toUpperCase()}</div><div>${time}</div>`, {
-        permanent: true, // ラベルを常時表示
-        direction: index === 3 ? 'top' : 'right', // ラベルの表示位置を設定
-        offset: [0, index === 3 ? -10 : 10], // ラベルの位置を微調整
-        className: 'custom-marker' // ラベルのスタイルを設定するCSSクラス名
-    });
-});
-
-
-
-// 各地点のタイムゾーンを取得する
-var timeZones = [];
-for (var i = 0; i < country.length; i++) {
-    timeZones.push(getTimeZone(country[i]));
-}
-
-// 各地点の現在時刻を取得する
-var times = [];
-for (var i = 0; i < timeZones.length; i++) {
-    var time = new Date().toLocaleTimeString("ja-JP", { timeZone: timeZones[i], hour12: false, hour: '2-digit', minute: '2-digit', seconds: false });
-    times.push(time);
 }
